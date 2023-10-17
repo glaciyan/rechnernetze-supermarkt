8586f1fc42f775ba872d0d654a09ab3b8e29d6f1
Kevin T <30321316+glaciyan@users.noreply.github.com>
2023-10-17 15:07:27 +0200

    working real time

--- RealTimeSimSkeleton.py
+++ RealTimeSimSkeleton.py
@@ -6,7 +6,7 @@ import time
 f = open("supermarkt.txt", "w")
 fc = open("supermarkt_customer.txt", "w")
 fs = open("supermarkt_station.txt", "w")
-timeFactor = 1
+timeFactor = 0.1
 
 
 # print on console and into supermarket log
@@ -62,6 +62,7 @@ class Station(Thread):
 
         waitTime = self.delay_per_item * customer.einkaufsliste[customer.current][2]
         time.sleep(waitTime * timeFactor)
+        print(customer.name, "served", self.name)
         customer.servEv.set()
 
 
@@ -75,19 +76,47 @@ class Customer(Thread):
     duration = 0
     duration_cond_complete = 0
     count = 0
-    finished = False
 
-    def __init__(self, einkaufsliste, name):
+    def __init__(self, einkaufsliste, name, startTime):
         Thread.__init__(self)
         self.einkaufsliste = einkaufsliste
         self.name = name
+        self.startTime = startTime
         self.servEv = threading.Event()
         self.current = 0
+        self.finished = False
+        self.timeStep = self.startTime
         Customer.count += 1
 
     def run(self):
-        while not Customer.finished:
-            time.sleep(self.einkaufsliste[self.current][0])
+        time.sleep(self.startTime * timeFactor)
+
+        while not self.finished:
+            if self.current >= len(self.einkaufsliste):
+                self.finished = True
+                print(self.name, "done")
+                break
+
+            time.sleep(self.einkaufsliste[self.current][0] * timeFactor)
+
+            station: Station = self.einkaufsliste[self.current][1]
+
+            capacity = self.einkaufsliste[self.current][3]
+            if len(station.buffer) > capacity:
+                self.current += 1
+                continue
+
+            station.buffer_lock.acquire()
+            station.buffer.append(self)
+            station.buffer_lock.release()
+
+            station.CustomerWaitEv.set()
+            print(self.name, "ankuft", station.name)
+
+            self.servEv.wait()
+            self.servEv.clear()
+
+            self.current += 1
 
 
 # please implement here
@@ -99,6 +128,7 @@ def startCustomers(einkaufsliste, name, startTime, newCustomerTime, mT):
     t = startTime
     while t < mT:
         kunde = Customer(list(einkaufsliste), name + str(i), t)
+        kunde.start()
         # ev = Ev(t, kunde.run, prio=1)
         # evQ.push(ev)
         i += 1
@@ -106,9 +136,13 @@ def startCustomers(einkaufsliste, name, startTime, newCustomerTime, mT):
 
 
 baecker = Station(10, 'Bäcker')
+baecker.start()
 metzger = Station(30, 'Metzger')
+metzger.start()
 kaese = Station(60, 'Käse')
+kaese.start()
 kasse = Station(5, 'Kasse')
+kasse.start()
 Customer.served['Bäcker'] = 0
 Customer.served['Metzger'] = 0
 Customer.served['Käse'] = 0
@@ -121,6 +155,7 @@ einkaufsliste1 = [(10, baecker, 10, 10), (30, metzger, 5, 10), (45, kaese, 3, 5)
 einkaufsliste2 = [(30, metzger, 2, 5), (30, kasse, 3, 20), (20, baecker, 3, 20)]
 startCustomers(einkaufsliste1, 'A', 0, 200, 30 * 60 + 1)
 startCustomers(einkaufsliste2, 'B', 1, 60, 30 * 60 + 1)
+time.sleep(1000)
 # my_print('Simulationsende: %is' % EvQueue.time)
 my_print('Anzahl Kunden: %i' % (Customer.count
                                 ))
